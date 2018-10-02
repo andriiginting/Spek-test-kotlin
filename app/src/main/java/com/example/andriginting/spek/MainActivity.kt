6b0a4be574e3b1be7b70c74656499a52ae03287d
Andri Suranta Ginting <andris.ginting@gmail.com>
2018-10-02 13:19:54 +0700

    [andri] implement presenter in mainActivity

--- app/src/main/java/com/example/andriginting/spek/MainActivity.kt
+++ app/src/main/java/com/example/andriginting/spek/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.andriginting.spek
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
@@ -7,33 +8,44 @@ import android.view.Menu
 import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ShapeContract.View {
+
+    private var presenter: ImpPresenter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        presenter = ImpPresenter(this)
+
+        fab.setOnClickListener {
+            presenter?.calculateShape(height = height_item.text.toString().toInt()
+                    , width = widht_item.text.toString().toInt())
         }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun showResult(result: Int) {
+        calculate_result.text = resources.getString(R.string.result_hint) + result.toString()
+    }
+
+    override fun emptyField() {
+        height_item.text.clear()
+        widht_item.text.clear()
     }
 }
